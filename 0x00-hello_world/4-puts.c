8d46503463210438b8ce4f4af6d634f89fed1fa1
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-08 11:54:07 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put function
  * 
@@ -6,7 +7,8 @@
  * this program print "Programming is like building a multilingual puzzle
  * Return: 0 
  */
-int main(void);
+
+int main(void)
 {
            puts("\"programming is like buliding a multilingual puzzle");
            return (0);
