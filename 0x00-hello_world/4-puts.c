aad43aa7aea4b0bbe7bc867791abeb21ef5778a5
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-11 02:53:31 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
+ 
  * Return: 0 after printing the function
  */
 int main(void)
 {puts("\"programming is like buliding a multilingual puzzle");
-	(return(0));
+	return(0);
 }
