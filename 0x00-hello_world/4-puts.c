69d8f12215a8bc45fc710bf7df37f532198a269f
MA121010IBRO <email4mame@gmail.com>
2022-09-11 02:12:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
  * Return: 0 after printing the function
  */
 int main(void)
 {puts("\"programming is like buliding a multilingual puzzle");
-        return (0);
+	(return(0));
 }
