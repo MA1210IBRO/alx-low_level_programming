9c231edadee16b01b064220f2babb23c7c70eb8e
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-12 00:54:44 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programming is like buliding a multilingual puzzle");
+{
+	puts("\"programming is like buliding a multilingual puzzle");
 	return(0);
 }
