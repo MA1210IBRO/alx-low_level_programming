7c916feee4a2a408c75a0b7c9125030f9712a500
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-12 08:22:40 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 
-	dprintf(STDERR_FILENO,"size of a char: %d byte(s)\n", sizeof(char));
-	dprintf(STDERR_FILENO,"size of an int: %d byte(s)\n", sizeof(int));
-	dprintf(STDERR_FILENO,"size of a long int: %d byte(s)\n", sizeof(long int));
-	dprintf(STDERR_FILENO,"size of a long long int: %d byte(s)\n", sizeof(long long int));
-	dprintf(STDERR_FILENO,"size of a float: %d byte(s)\n", sizeof(float));
+	fprintf(stderr,"size of a char: %d byte(s)\n", sizeof(char));
+	fprintf(stderr,"size of an int: %d byte(s)\n", sizeof(int));
+	fprintf(stderr,"size of a long int: %d byte(s)\n", sizeof(long int));
+	fprintf(stderr,"size of a long long int: %d byte(s)\n", sizeof(long long int));
+	fprintf(stderr,"size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
