2b014e37b143cecfc176c5fb9692c2193c698e57
MA121010IBRO <email4mame@gmail.com>
2022-09-14 06:05:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,16 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remanider;
-	int mins_remainder;
+	int mins_remanider;
 
 	while (hours <= 23)
 	{
 	while (minutes <= 59)
 	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
+	mins_remanider = minutes % 10;
+	hours_remanider = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_reminder + '0')
+	_putchar(hours_remanider + '0')
 	_putchar(':');
 	_putchar(minutes / 10 + '0')
 	_putschar(mins_remanider + '0');
