52a665c266e7b9dfa159b25782de95d16334ee77
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-16 01:24:18 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,15 @@
  * Return: 1 whether it is, 0 otherwise
  */
 
-int _isupper(int c)
+int _isupper(int c);
 {
 	if ((c >= 'A') && (c <= 'Z'))
 	{
-		return (1);
+	return (1);
 	}
-
+	else
+	{
 	return (0);
+	}
 }
 
