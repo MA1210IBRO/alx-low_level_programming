bf00df02f2ad5a4dfba6ce37757a75b3ffdedabc
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-17 00:22:51 -0700

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void prints_square(int size)
 
 	if (size > 0)
 	{
-		for (inc1 = 0; inc1 < (size - 1); inc1++)
+		for (inc1 = 0; inc1 < size; inc1++)
 		{
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
