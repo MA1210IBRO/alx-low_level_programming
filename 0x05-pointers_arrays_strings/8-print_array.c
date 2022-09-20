9c691f92d79909579267b04a47f9c96a44808db6
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-09-20 07:02:50 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,13 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-	for (i = 0; i < n ; i++)
+	int inc;
+	for (inc = 0; inc < n ; inc++)
 	{
-		printf("%d, ", a[i]);
-		
-		if(i != (n - 1))
-		{
-			printf(", ");
-		}
+		if (inc != n - 1)
+			printf("%d, ", a[inc]);
+		else
+			printf("%d", a[inc]);
 	}
-	putchar("\n");
+	putchar("10");
 }
