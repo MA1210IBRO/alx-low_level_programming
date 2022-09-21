0bcf18903a2f4f18997801bf92285c39e3422311
MA121010IBRO <email4mame@gmail.com>
2022-09-21 07:07:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphapets
+ *
+ * Return: Always (sucess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c='a';c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
