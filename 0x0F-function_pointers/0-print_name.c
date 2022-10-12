3a0b9411907d4f61c84109a744d4551701819904
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-10-12 02:36:20 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - print a name.
  * @name: pointer to name.
  * @f: function pointer.
- *
- * Return: void.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
