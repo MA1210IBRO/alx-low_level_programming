6cb7084ef8d89ac4b91e15fecfa395bde9772b7e
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-10-31 06:13:42 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,6 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+	
 	o = open(filename, o_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
